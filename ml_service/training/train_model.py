5a4fabe1c601b4ebde088a1ffff6d1dca74c6418
Your Name <nirajkumar09160@gmail.com>
2026-04-02 00:56:11 +0530

    done prediction

--- ml_service/training/train_model.py
+++ ml_service/training/train_model.py
@@ -6,9 +6,9 @@ os.makedirs("../model", exist_ok=True)
 
 # Example dataset (replace with CSV later)
 data = pd.DataFrame({
-      'income': [20000, 50000, 30000, 80000, 100000],
-      'age': [25, 45, 35, 50, 40],
-      'loan': [0, 1, 0, 1, 1]
+      'income': [20000, 50000, 30000, 80000, 100000,40000],
+      'age': [25, 45, 35, 50, 40, 35],
+      'loan': [0, 1, 0, 1, 1, 1]
 })
 
 X = data[['income', 'age']]
